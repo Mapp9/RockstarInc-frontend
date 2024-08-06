0c448d037a9deeec20b1b03003f8116733c56f3e
Mapp9 <mpp.adso@gmail.com>
2024-08-05 23:15:01 -0500

    Se corrige error en el archivo app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { SignupComponent } from './signup/signup.component';
 const routes: Routes = [
   { path : "login", component: LoginComponent},
   { path : "signup", component: SignupComponent},
-  { path: 'costumer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+  { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
 
 @NgModule({
